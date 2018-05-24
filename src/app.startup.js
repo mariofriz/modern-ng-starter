445299724b4ce11824691e044de9685328790057
Mario Friz <mario.friz@non.schneider-electric.com>
2018-05-24 14:51:32 +0200

    build(dep): add ng-bus dependency

--- src/app.startup.js
+++ src/app.startup.js
@@ -3,16 +3,17 @@ import './i18n/translations_de.po';
 import './i18n/translations_en.po';
 
 // let's start up that app
-export default function startup(gettextCatalog) {
+export default function startup($bus, gettextCatalog) {
   if (process.env.NODE_ENV !== 'production') {
     console.log('Development mode is on');
 
     // enable gettext debug in dev mode
     gettextCatalog.debug = true;
+    $bus.enableDebugMode();
   }
 
   // set language to use for ui
   gettextCatalog.setCurrentLanguage('en');
 }
 
-startup.$inject = ['gettextCatalog'];
+startup.$inject = ['$bus', 'gettextCatalog'];
